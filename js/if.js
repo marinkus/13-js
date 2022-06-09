18e9d99e9e71f77c24af4bcae9379b7cda791bc6
marinkus <marinkus@gmail.com>
2022-06-09 10:41:33 +0300

    nested ifs and logical operators

--- js/if.js
+++ js/if.js
@@ -14,6 +14,11 @@ visi: > < >= <= == != ===, !==
 naudotini: > < >= <= ===, !==
 nenaudotini: == !=
 
+
+Loginiai operatoriai
+&& (and) - turi tenkinti visos salygos
+|| (or) - turi tenkinti bent viena salyga
+
 */
 
 const a = 3;
@@ -44,3 +49,76 @@ if (x === 5) {
 else {
     console.log('NE');
 }
+
+
+const diena = 5;
+if (diena === 1) {
+    console.log('pirmadienis');
+} else {
+    if (diena === 2) {
+        console.log('antradienis');
+    } else {
+        if (diena === 3) {
+        console.log('treciadienis');
+    } else {
+        if (diena === 4) {
+            console.log('ketvirtadienis');
+        } else {
+            if (diena === 5) {
+                console.log('penktadienis');
+            }
+        }
+    }
+}
+}
+
+console.log('--------------------------------------');
+
+const day = 5;
+if (day === 1) {
+    console.log('pirmadienis')
+} else if (day === 2) {
+    console.log('antradienis');
+} else if (day === 3) {
+    console.log('treciadienis');
+} else if (day === 4) {
+    console.log('ketvirtadienis');
+} else if (day === 5) {
+    console.log('penktadienis');
+} else if (day === 6) {
+    console.log('sestadienis');
+} else if (day === 7) {
+    console.log('sekmadienis');
+} else {
+    console.log('Tokia diena savaiteje neegzistuoja');
+}
+
+console.log('------------------BREAK-------------');
+
+const temperatura = 18;
+const lietus = true;
+const nuoKadaSilta = 12;
+
+if (lietus === true) {
+    console.log('pasiimk sketi');
+} else {
+    if (temperatura >= nuoKadaSilta) {
+    console.log('gali eiti su maike');
+    } else {
+        console.log('pasiimk striuke');
+    }
+}
+console.log('------------------&& || -------------');
+if (lietus === true && temperatura >= nuoKadaSilta) {
+    console.log('2) varom nuogi');
+} else if (lietus === true && temperatura < nuoKadaSilta) {
+    console.log('2) pasiimk palta ir sketi');
+} else if (lietus === false && temperatura >= nuoKadaSilta) {
+    console.log('2) eik su maike');
+} else if (lietus === false && temperatura < nuoKadaSilta) {
+    console.log('2) vistiek pasiimk striuke');
+} else {
+    console.log('nesamone...');
+} // jeigu vietoj true/false suvedam lempa 'kkdsf0302'
+
+console.log('------------------````` ------------');
