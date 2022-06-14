89d22848278bebdfd68b48ad5b1e5da02dc66398
marinkus <marinkus@gmail.com>
2022-06-14 10:43:30 +0300

    if, for, push Array

--- js/for.js
+++ js/for.js
@@ -28,3 +28,39 @@ for (let i = 0; i < word.length; i++) {
     const letter = word[i];
     console.log(i, ')', letter);
 }
+
+console.clear();
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+    if (i === 5) {
+        break;
+    }
+}
+console.log('----------');
+
+const money = [5, 10, 20, 50, 100, 5, 10, 20];
+const minTarget = 80;
+const hand = [];
+let handTotal = 0;
+
+for (let i = 0; i < money.length; i++) {
+    const value = money[i];
+    handTotal += value;
+    hand.push(value)
+    console.log(value);
+    if (handTotal >= minTarget) {
+        break;
+    }
+}
+console.log(handTotal)
+
+const graza = handTotal - minTarget;
+if (graza > 0) {
+    console.log(`Graza: ${graza}`);
+    console.log(`Duotos kupiuros ${hand}`);
+}
