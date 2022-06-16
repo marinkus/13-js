8e107a82007850f53cefb8f8f72e45038c877119
marinkus <marinkus@gmail.com>
2022-06-16 11:49:06 +0300

    truphy falsy values

---
+++ js/if-values.js
@@ -0,0 +1,27 @@
+console.clear();
+
+/*
+REIKSMIU POZITYVUMAS / NEGATYVUMAS
+    Skaiciai:
+    0 - negatyvus
+    kiti - pozityvus
+boolean:
+    true - posityvus
+    false - negatyvus
+string:
+    tuscias - negatyvus
+array:      []
+    visada pozityvus
+object:     {}
+    visada pozityvus
+funkcijos: () => {}
+    visada pozityvus
+*/
+
+const userInput = '';
+
+if (userInput) {
+    console.log('taip');
+} else {
+    console.log('ne');
+}
