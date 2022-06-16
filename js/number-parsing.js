9a237e4270af22de7b7593fd006e7b4e264b2c8d
marinkus <marinkus@gmail.com>
2022-06-16 10:53:24 +0300

    toString

--- js/number-parsing.js
+++ js/number-parsing.js
@@ -1,6 +1,9 @@
 const n1 = '7';
 const n2 = 5;
 function suma(a, b) {
+    const initialASize = ('' + a).length;
+    const initialBSize = ('' + b).length; // toString **
+console.log(initialASize);
     if (typeof a !== 'number') 
     {a = parseInt(a)}
     if (typeof b !== 'number') 
