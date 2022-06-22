4591bedf26b37098f2fb40e355be918777364e9d
marinkus <marinkus@gmail.com>
2022-06-22 10:29:00 +0300

    recursion

--- js/family.js
+++ js/family.js
@@ -33,7 +33,7 @@ const family = [
     },
 ];
 
-function oldest(list) {
+function oldest(list, depth = 0) {
     let age = 0;
     let name = '';
     for (const person of list) {
@@ -42,17 +42,15 @@ function oldest(list) {
             name = person.name;
         }
         if (person.children && person.children.length > 0) {
-            const oldestChild = oldest(person.children);
-            const parts = oldestChild.split(' ');
-            const childName = parts[0];
-            const childAge = +parts[2];
-            if (childAge > age) {
-                age = childAge;
-                name = childName;
+            const oldestChild = oldest(person.children, depth+1);
+            if (oldestChild.age > age) {
+                age = oldestChild.age;
+                name = oldestChild.name
             }
         }
     }
-    return `${name} is ${age} years old`;
+    console.log(depth, {name, age});
+    return depth === 0 ? `${name} is ${age} years old` : {name, age}
 }
 
 console.log(oldest(family)); //Rimagardas is 140 years old
